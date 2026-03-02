6bde5c0467acbfffabaf115b8f923cd9ed4a89c6
ShriRadhey-Mishra <radheymishra13@gmail.com>
2026-03-02 16:02:12 +0530

    Updated a Greetings Controller

--- src/main/kotlin/com/kotlinspring/controller/GreetController.kt
+++ src/main/kotlin/com/kotlinspring/controller/GreetController.kt
@@ -1,6 +1,7 @@
 package com.kotlinspring.controller
 
 import com.kotlinspring.service.GreetingsService
+import mu.KLogging
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/greet")
 class GreetController(val greetingsService: GreetingsService) {
+
+    companion object: KLogging()
+
     @RequestMapping("/")
     fun greet(): String {
         return "Hello, World!"
@@ -15,6 +19,7 @@ class GreetController(val greetingsService: GreetingsService) {
 
     @RequestMapping("/{name}")
     fun greetWithName(@PathVariable("name") name: String): String {
+        logger.info("Name: $name")
         return greetingsService.retrieveGreeting(name)
     }
 }
