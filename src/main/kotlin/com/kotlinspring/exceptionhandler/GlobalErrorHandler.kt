39aa1db946ed3f56f5e53f329d5efb85d614a83a
ShriRadhey-Mishra <radheymishra13@gmail.com>
2026-03-03 19:25:25 +0530

    added a global exception handler

---
+++ src/main/kotlin/com/kotlinspring/exceptionhandler/GlobalErrorHandler.kt
@@ -0,0 +1,42 @@
+package com.kotlinspring.exceptionhandler
+
+import mu.KLogging
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@Component
+@ControllerAdvice
+class GlobalErrorHandler : ResponseEntityExceptionHandler() {
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        logger.error("MethodArgumentNotValidException observed: ${ex.message}", ex)
+        val errors = ex.bindingResult.allErrors
+            .map { err -> err.defaultMessage!! }
+            .sorted()
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors)
+    }
+
+    override fun handleExceptionInternal(
+        ex: Exception,
+        body: Any?,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        logger.error("Exception observed: ${ex.message}", ex)
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred")
+    }
+
+}
