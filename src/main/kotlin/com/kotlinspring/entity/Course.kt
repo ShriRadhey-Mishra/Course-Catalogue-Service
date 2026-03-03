18b918fc55a92df3b48162fd0e0c11b9279538f1
ShriRadhey-Mishra <radheymishra13@gmail.com>
2026-03-03 17:56:57 +0530

    created a course entity

---
+++ src/main/kotlin/com/kotlinspring/entity/Course.kt
@@ -0,0 +1,17 @@
+package com.kotlinspring.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Table
+import jakarta.persistence.Id
+
+@Entity
+@Table(name = "courses")
+data class Course(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Int?,
+    val name: String,
+    val category: String
+)
