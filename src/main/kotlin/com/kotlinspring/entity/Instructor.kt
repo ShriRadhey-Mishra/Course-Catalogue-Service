3021de6ab6655729ba969060d441eb303a51c982
ShriRadhey-Mishra <radheymishra13@gmail.com>
2026-03-05 15:38:28 +0530

    add Instructor entity with id and name fields

---
+++ src/main/kotlin/com/kotlinspring/entity/Instructor.kt
@@ -0,0 +1,16 @@
+package com.kotlinspring.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "Instructors")
+data class Instructor(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int?,
+    var name: String
+)
